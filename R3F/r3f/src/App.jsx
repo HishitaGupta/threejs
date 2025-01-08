48058f04fd5faab0d6584e36914dd438104351e1
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2025-01-08 17:49:06 +0530

    drei

--- R3F/r3f/src/App.jsx
+++ R3F/r3f/src/App.jsx
@@ -1,24 +1,70 @@
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+
+const Cube = ({ position, color, size }) => {
+
+  const ref=useRef()
+
+  useFrame((state,delta)=>{
+    ref.current.rotation.y+=delta
+    // delta is the time between frames (current frame - previous frame)
+    
+    
+  })
+
+
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      {/* x,y,z */}
+      <meshStandardMaterial color={color} />
+    </mesh>
+  )
+}
+
+const Sphere =({position,color,size})=>{
+
+  const[ishover,setishover]=useState(false)
+
+  const circleref=useRef()
+
+  useFrame((state,delta)=>{
+    circleref.current.rotation.y+=delta
+    // delta is the time between frames (current frame - previous frame)
+    
+    
+  })
+
+
+  return(
+    <mesh position={position} ref={circleref} onPointerEnter={(event)=>setishover(true)} onPointerLeave={(event)=>setishover(false)}>
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={`${ishover?'red':'green'}`} wireframe={true}/>
+    </mesh>
+  )
+}
 
 const App = () => {
   return (
     <Canvas className='bg-black'>
       <directionalLight position={[10, 10, 10]} />
+      <ambientLight intensity={0.5} />
+
+      {/* <group>
+
+      <Cube position={[1, 0, 0]} color='orange' size={[1, 1, 1]} />
+
+      <Cube position={[0, 2, 0]} color='red' size={[1, 1, 1]} />
+
+      </group> */}
 
 
-      
-      <mesh position={[1, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} /> 
-        {/* x,y,z */}
-        <meshStandardMaterial color='orange' />
-      </mesh>
+    <Cube position={[0, 2, 0]} color={'red'} size={[1, 1, 1]} />
 
-      <mesh position={[-1, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} /> 
-        {/* x,y,z */}
-        <meshStandardMaterial color='orange' />
-      </mesh>
+    <Sphere position={[0, 0, 0]} color={'blue'} size={[1, 30, 30]} />
+
+   <OrbitControls   />
     </Canvas>
   )
 }
